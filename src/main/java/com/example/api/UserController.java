0ec6bbf2edcfe6bb890a5d57400f9d1e5c0b3bd7
Lakshitha Herath <lakshitha@Lakshithas-MacBook-Pro.local>
2017-03-25 21:36:55 +0530

    POST request added

--- src/main/java/com/example/api/UserController.java
+++ src/main/java/com/example/api/UserController.java
@@ -2,9 +2,14 @@ package com.example.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.User;
+
+
 
 
 
@@ -16,6 +21,14 @@ public class UserController {
 	public ResponseEntity<String> getUsers()
 	{
 		return new ResponseEntity<String>("Hello User",HttpStatus.OK);
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.POST, path = "/user")
+	public ResponseEntity<User> greetUsers(@RequestBody User user)
+	{
+		user.setAddress("This value set from Backend Server");
+		return new ResponseEntity<User>(user,HttpStatus.OK);
 	}
 	
 
